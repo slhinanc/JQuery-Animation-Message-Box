9d95849df1ab07d4f88323bdce589a2dba21f294
Salih İnanç <105978929+slhinanc@users.noreply.github.com>
2023-02-05 14:58:36 +0300

    JQuery Animation Message Box

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+$(function(){
+
+    $("#header").click(function(){
+        $("#content").toggle("fast");
+        $("#warn").hide("fast");
+    });
+
+    $("#close").click(function(){
+        $("#message-box").hide("fast");
+    });
+
+   
+
+
+});
